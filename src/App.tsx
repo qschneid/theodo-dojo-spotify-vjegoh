f6b9622681f3cdfb4b612c6c7aa325cb7617a196
qschneid <133768216+qschneid@users.noreply.github.com>
2023-05-31 21:20:58 +0200

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import logo from './assets/logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchTracks } from './lib/fetchTracks';
+import { SavedTrack } from 'spotify-types';
 
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
@@ -9,11 +12,18 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
+
 const App = () => {
   const [trackIndex, setTrackIndex] = useState(0);
   const goToNextTrack = () => {
     setTrackIndex(trackIndex + 1);
   };
+  const { data: tracks } = useQuery({
+    queryKey: ['tracks'],
+    queryFn: fetchTracks,
+  });
+  console.log(tracks);
+  const nb_title = tracks?.length;
 
   return (
     <div className="App">
@@ -28,6 +38,8 @@ const App = () => {
 
       <audio src={trackUrls[trackIndex]} autoPlay controls />
       <button onClick={goToNextTrack}>Next track</button>
+      <hr></hr>
+      <p>Il y a {nb_title} titres dans la playliste</p>
     </div>
   );
 };
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,19 @@
-const apiToken: string = '';
+import { useQuery } from '@tanstack/react-query/build/lib/useQuery';
+
+const apiToken: string =
+  'BQD2v0403xZQNnXb2e00aBhiwuY-nW8C1atigP8jd-yxndN5WeRDnSXmbEL9ConrT0jlCWdrHEXrWE8CUbcgtXYOsAFsJFb5sJq4m3b-K_rg-2MgNVjhZwCzaqxNd2einUSKFajkV-h32roa9deJsgjuw4gW2PXzP8PwRhJcNPI4Shm_KzPVxWrntPp36NJq0_M2g2bQHBTZ4bwgIrcVIw';
 
 export const fetchTracks = async () => {
-  // This function does nothing, yet
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: unknown[] };
+
+  return data.items;
 };
